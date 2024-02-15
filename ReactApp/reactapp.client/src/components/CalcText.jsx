7f23abea87faa89d83da4a697e58acd4807f149d
Thea Urke Remøy <thea.u.remoy@ntnu.no>
2024-02-15 20:56:12 +0000

    Merged PR 3376: Added CalcText Component
    Added the CalcText component and added images for that component.
    
    Related work items: #13444

---
+++ ReactApp/reactapp.client/src/components/CalcText.jsx
@@ -0,0 +1,12 @@
+
+export default function CalcText() {
+    return (
+        <>
+            <img src='../public/accounting.png' alt='calculating' />
+            <h6>Calculate the estimated <br />
+                emissions on your <br />
+                Azure resources</h6>
+            <img src='../public/arrow.png' alt='arrow' />
+        </>
+    );
+}
