8e69b8ee2e0958bb5c0a3ffea11d79f751fbe583
Marthe Sjaaeng <marthsja@stud.ntnu.no>
2024-02-26 18:51:02 +0000

    Merged PR 3408: Changed buttons to tabs, except start new calc. Added styling to navbar and n...
    Changed buttons to tabs, except start new calc. Added styling to navbar and navbutton.
    However, did not add delete button to tabs.
    
    Related work items: #13615

--- ReactApp/reactapp.client/src/components/Navbar.jsx
+++ ReactApp/reactapp.client/src/components/Navbar.jsx
@@ -2,15 +2,38 @@ import NavButton from './NavButton';
 import ToggleButton from './ToggleButton';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Tab from 'react-bootstrap/Tab';
+import Nav from 'react-bootstrap/Nav';
+import styles from '../styles/Navbar.module.css'
+import { useState } from 'react';
 
 
 export default function Navbar() {
+    const [activeTab, setActiveTab] = useState(''); // Initialize active tab state
+
+
     return (
-        <Container fluid style={{ height: '100%', display: 'flex', flexDirection: 'column'}}>
-            <Row style={{ flex: '8', padding: '0.1em' }}>
-                <NavButton text='Start new calculation' src='plusWhite.png' alt='New calculation'/>
+        <Container fluid className={styles.container}>
+            <Row style={{ flex: '1', padding: '0.1em' }}>
+                <NavButton text='Start new calculation' src='plusWhite.png' alt='New calculation' />
             </Row>
-            <Row style={{ flex: '1' } }>
+
+            <Row style={{flex: '9', padding: '0.1em'}}>
+                <Tab.Container activeKey={activeTab} onSelect={(key) => setActiveTab(key)}>
+                    <Col style={{padding: '0'} }>
+                        <Nav variant='pills' className={styles.navContainer}>
+                            <Nav.Item className={styles.navItem}>
+                                <Nav.Link eventKey='first' className={`${styles.navLink} ${activeTab === 'first' ? styles.activeTab : ''}`}>
+                                    Tab 1
+                                </Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Col>
+                </Tab.Container>
+            </Row>
+
+            <Row style={{flex: '1'}}>
                 <ToggleButton />
             </Row>
         </Container>
