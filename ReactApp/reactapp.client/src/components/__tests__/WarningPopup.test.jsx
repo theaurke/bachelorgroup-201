2c24be88bdb63a0183598e8169c1da54f40723bd
Marthe Sjaaeng <marthsja@stud.ntnu.no>
2024-03-10 21:38:39 +0000

    Merged PR 3460: test warning text and confirm and cancle function call
    test warning text and confirm and cancle function call
    
    Related work items: #13833

---
+++ ReactApp/reactapp.client/src/components/__tests__/WarningPopup.test.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import WarningPopup from '../WarningPopup.jsx';
+import '@testing-library/jest-dom';
+
+
+describe('WarningPopup Component Test', () => {
+    test('renders warning message correctly', () => {
+        const warningMessage = 'Test warning';
+        const { getByText } = render(<WarningPopup warning={warningMessage} />);
+        expect(getByText(warningMessage)).toBeInTheDocument();
+    });
+
+    test('calls onConfirm handler when "Yes" button is clicked', () => {
+        const onConfirmMock = jest.fn();
+        const { getByText } = render(<WarningPopup warning='Test warning' onConfirm={onConfirmMock} onCancel={() => { }} />);
+        fireEvent.click(getByText('Yes'));
+        expect(onConfirmMock).toHaveBeenCalled();
+    });
+
+    test('calls onCancel handler when "Cancel" button is clicked', () => {
+        const onCancelMock = jest.fn();
+        const { getByText } = render(<WarningPopup warning='Test warning' onConfirm={() => { }} onCancel={onCancelMock} />);
+        fireEvent.click(getByText('Cancel'));
+        expect(onCancelMock).toHaveBeenCalled();
+    });
+
+});
