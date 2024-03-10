28d0c900dc1ac346f10e8d6e7dd6282d1a2c8ed8
Marthe Sjaaeng <marthsja@stud.ntnu.no>
2024-03-10 14:58:07 +0000

    Merged PR 3457: Test tooltip ToggleButton
    Test tooltip ToggleButton
    
    Related work items: #13827

--- ReactApp/reactapp.client/src/components/ToggleButton.jsx
+++ ReactApp/reactapp.client/src/components/ToggleButton.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Col from 'react-bootstrap/Col';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
---
+++ ReactApp/reactapp.client/src/components/__tests__/ToggleButton.test.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import ToggleButton from '../ToggleButton.jsx';
+import '@testing-library/jest-dom';
+
+
+describe('ToggleButton Component Test', () => {
+    test('renders button with tooltip', async () => {
+        const toggleSidebar = jest.fn();
+
+        const { getByAltText, getByText } = render(<ToggleButton toggleSidebar={toggleSidebar} isWindowSmall={false} />);
+
+        const button = getByAltText('toggleSidebar');
+        expect(button).toBeInTheDocument();
+
+
+        fireEvent.mouseEnter(button);
+        await waitFor(() => {
+            expect(getByText('Toggle Sidebar')).toBeInTheDocument();
+        });
+        
+        fireEvent.click(button);
+        expect(toggleSidebar).toHaveBeenCalled();
+    });
+
+    test('renders button with "Show Calculations" tooltip when isWindowSmall is true', async () => {
+        const toggleSidebar = jest.fn();
+
+        const { getByAltText, getByText } = render(<ToggleButton toggleSidebar={toggleSidebar} isWindowSmall={true} />);
+
+        const button = getByAltText('toggleSidebar');
+        expect(button).toBeInTheDocument();
+
+
+        fireEvent.mouseEnter(button);
+        await waitFor(() => {
+            expect(getByText('Show Calculations')).toBeInTheDocument();
+        });
+    });
+    
+});
